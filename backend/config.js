379676c5e15d6a2fa36b2d24c9d6b775f752b349
Triffycodes <adarsh.s.gowda20@gmail.com>
2025-04-20 19:37:00 +0530

    add middlewares(cors, body parser) and signin and signup

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+module.exports ={
+    JWT_SECRETS: "your_jwt_secret",
+}
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const mainRouter = require('./router/index');
+const cors = require("cors");
+
+app.use(cors());
+
 const app = express();
+app.use(express.json());
 
 
 app.use('/api/v1', mainRouter);
+
+app.listen(3000);
